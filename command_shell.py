d034b6b46e8d46305cbec3f02a3d834427eddcb7
MAGANER <maganer.krik@mail.ru>
2023-12-28 12:16:01 +0300

    decorated run function to prevent interruption. Minor commenting

--- command_shell.py
+++ command_shell.py
@@ -1,16 +1,15 @@
 import sys
 import os
 
-
 class CommandShell:
     def __init__(self):
-
         #every command is repsented with lambda that takes list of argument(even if command doesn't require any additional data)
         self.commands = {
             "quit": lambda args: sys.exit()
         }
-        
+    
     def __run_interactively(self):
+        ''' get user's input, then process command and print response'''
         while True:
             command = input(">>")
             if command not in self.commands.keys():
@@ -19,12 +18,26 @@ class CommandShell:
                 command, *arguments = command.split(" ")
                 self.commands[command](arguments)
     def __run_cli(self):
+        '''parse command line arguments, execute command and quit then'''
         pass
 
+    def guard(fn):
+        '''decorator to catch ctrl-C and other interruptions'''
+        def inner(self):
+            try:
+                fn(self)
+            except KeyboardInterrupt:
+                sys.exit()
+                os.clear()
+        return inner
+    
 
+    @guard
     def run(self):
+        '''the only public method. Call it to run app'''
         if len(sys.argv) > 1:
             self.__run_cli()
         else:
             self.__run_interactively()
+
         
