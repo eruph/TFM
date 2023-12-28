1c75ed3c6dbf8fff64b584d2e9990248f76ee98c
MAGANER <maganer.krik@mail.ru>
2023-12-28 13:39:34 +0300

    added improvement on how application is invoked

--- __main__.py
+++ __main__.py
@@ -1,4 +1,10 @@
 if __name__ == '__main__':
     from command_shell import *
+
+    #TODO: there is a chance chdir may throw exception, so check it
+    #to make available using python TFM
+    from os import chdir
+    chdir("TFM")
+    
     shell = CommandShell()
     shell.run()
