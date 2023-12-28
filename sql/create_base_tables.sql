f459f6017a524536c2dea45f919b9e1f4c3b72aa
MAGANER <maganer.krik@mail.ru>
2023-12-28 13:22:49 +0300

    added script to create base tables such as tagless and tags

---
+++ sql/create_base_tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tagless
+(
+	id INTEGER PRIMARY KEY,
+	path TEXT NOT NULL,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE tags
+(
+	id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL,
+	parent TEXT
+);
