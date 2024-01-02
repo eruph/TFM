4860f0dbc2c7004646b0346f7ceb15f463f989b8
MAGANER <maganer.krik@mail.ru>
2024-01-02 23:14:32 +0300

    added function to print content of choosen table

--- common_operator.py
+++ common_operator.py
@@ -6,7 +6,8 @@ class Operator:
     '''class that provides interface to all operation that can be apllied to files/tagsx'''
     def __init__(self):
         self.commands = {
-            "scan": lambda args: self.crifp(args,1,self.scan)
+            "scan": lambda args: self.crifp(args,1,self.scan),
+            "list": lambda args: self.crifp(args,1,self.list_table_content)
         }
         self.__db = DBManager()
         
@@ -18,7 +19,7 @@ class Operator:
         else:
             fn(*args)
     
-    def scan(self,root: str):
+    def scan(self,root: str) -> None:
         '''scan example: scan root_directory'''
         #this functions scans passed directory and adds all its content to tagless table
         
@@ -39,4 +40,17 @@ class Operator:
                 self.__db.add_tagless_item(next_id,top,file)
                 next_id += 1
                 print_progress(top,file)
+
+
+    def list_table_content(self, table:str) -> None:
+        '''list example: list table_name'''
+
+        #TODO: do better output
+        prnt = lambda left, right: print("{} {} {}".format(left," "*10,right))
+        if self.__db.does_table_exist(table):
+            prnt(":path:",":name:")
+            for path, name in self.__db.get_table_content(table):
+                prnt(path,name)
+        else:
+            print("'{}' table doesn't exist!".format(table))
         
