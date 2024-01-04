add4f56fa5f627228962155db987f5383fbb9ad3
MAGANER <maganer.krik@mail.ru>
2024-01-04 18:53:21 +0300

    added method to get tags, made common method to get table max size

--- DBManager.py
+++ DBManager.py
@@ -9,6 +9,11 @@ class DBManager:
         self.cursor = self.connection.cursor()
         
         if self.is_first_start: self.__create_special_tables()
+
+        #load script to create table
+        with open("sql/create_tag_table.sql","r") as f:
+            self.tag_table_req = f.read()
+            
     def __del__(self):
         self.connection.close()
 
@@ -23,13 +28,16 @@ class DBManager:
             self.connection.commit()
         print("tfm.db has been created!")
 
-    def get_tagless_max(self) -> int:
-        self.cursor.execute("SELECT MAX(id) FROM tagless")
+    def _get_table_max(self, table:str) -> int:
+        '''common function to retrieve size of table'''
+        self.cursor.execute("SELECT MAX(id) FROM {}".format(table))
         result = self.cursor.fetchall()
         if result[0][0] is None:
             return 0
         else:
             return result[0][0]
+
+    def get_tagless_max(self) -> int: return self._get_table_max("tagless")
 
     def add_tagless_item(self,id:int,path:str,name:str) -> None:
         self.cursor.execute("INSERT INTO tagless (id, path, name) VALUES({}, '{}', '{}')".format(id,path,name))
@@ -47,6 +55,24 @@ class DBManager:
         '''returns list of pairs made up from strings'''
         self.cursor.execute("SELECT path, name FROM {}".format(table))
         return self.cursor.fetchall()
+
+
+    def create_tag(self, tag:str) -> None:
+        '''create new table associated with provided tag'''
+        #Note: check tag existence before creation
+        self.cursor.execute(self.tag_table_req.format(tag))
+        self.connection.commit()
+
+        #make a record there is such tag
+        tags_size = self._get_table_max("tags")
+        self.cursor.execute("INSERT INTO tags (id,name,parent) VALUES({},'{}','')".format(tags_size+1,tag))
+        self.connection.commit()
+
+    def get_tags(self) -> list:
+        '''return list of strings, containg names of all existing tags'''
+        self.cursor.execute("SELECT name from tags")
+        result = self.cursor.fetchall()
+        return [x[0] for x in result]
         
 
     
