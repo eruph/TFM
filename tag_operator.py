1e0ce384cd63431eca8a2cacc359a70ddd55caf8
MAGANER <maganer.krik@mail.ru>
2024-01-04 18:51:48 +0300

    added operations to create tag/show existing ones

--- tag_operator.py
+++ tag_operator.py
@@ -1,7 +1,22 @@
 from base_operator import BaseOperator
 
-
 class TagOperator(BaseOperator):
     def __init__(self):
         super().__init__()
-        self.commands = {}
+        self.commands = {
+            "new":lambda args: self.crifp(args,1,self.create_new_tag),
+            "lst":lambda args: self.crifp(args,0,self.show_existing_tags)
+        }
+
+    def create_new_tag(self, tag:str) -> None:
+        '''new example: new tag_name'''
+        if self.db.does_table_exist(tag):
+            print("{} tag is already created!")
+            return None
+
+        self.db.create_tag(tag)
+
+    def show_existing_tags(self) -> None:
+        '''lst example: lst'''
+        for tag in self.db.get_tags():
+            print(tag)
