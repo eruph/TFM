67804becb25fce980f06eac93e6c091432d73c52
MAGANER <maganer.krik@mail.ru>
2024-01-04 18:24:47 +0300

    inherit from base operator

--- common_operator.py
+++ common_operator.py
@@ -1,15 +1,15 @@
-from DBManager import *
+from base_operator import BaseOperator
 import os
 from typing import Callable, Any
 
-class Operator:
+class Operator(BaseOperator):
     '''class that provides interface to all operation that can be apllied to files/tagsx'''
     def __init__(self):
+        super().__init__()
         self.commands = {
             "scan": lambda args: self.crifp(args,1,self.scan),
             "list": lambda args: self.crifp(args,1,self.list_table_content)
         }
-        self.__db = DBManager()
         
     #check and run if possible:for short 
     def crifp(self,args:list, required_args_amount:int, fn:Callable[..., Any]) -> bool:
@@ -27,17 +27,17 @@ class Operator:
             print("{} doesn't exist!".format(root))
             return None
 
-        next_id = self.__db.get_tagless_max()
+        next_id = self.db.get_tagless_max()
 
         print_progress = lambda top, item: print("{}/{} is staged as tagless!".format(top,item))
         for top, dirs, files in os.walk(root):
             for dir in dirs:
-                self.__db.add_tagless_item(next_id,top,dir)
+                self.db.add_tagless_item(next_id,top,dir)
                 next_id += 1
                 print_progress(top,dir)
                 
             for file in files:
-                self.__db.add_tagless_item(next_id,top,file)
+                self.db.add_tagless_item(next_id,top,file)
                 next_id += 1
                 print_progress(top,file)
 
@@ -47,9 +47,9 @@ class Operator:
 
         #TODO: do better output
         prnt = lambda left, right: print("{} {} {}".format(left," "*10,right))
-        if self.__db.does_table_exist(table):
+        if self.db.does_table_exist(table):
             prnt(":path:",":name:")
-            for path, name in self.__db.get_table_content(table):
+            for path, name in self.db.get_table_content(table):
                 prnt(path,name)
         else:
             print("'{}' table doesn't exist!".format(table))
